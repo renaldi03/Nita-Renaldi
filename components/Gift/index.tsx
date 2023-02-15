5e225cec27441a519cdcbbe8f742f2f33bab6dac
renaldia63@gmail.com <renaldia63@gmail.com>
2023-02-15 10:44:31 +0700

    upload

--- components/Gift/index.tsx
+++ components/Gift/index.tsx
@@ -1,17 +1,17 @@
 export default function Gift() {
   return (
     <div className="gift">
-      <h1>Amplop Digital</h1>
-      <p>
+      <h1>Kirim Hadiah</h1>
+      {/* <p>
         Bagi keluarga dan sahabat yang ingin mengerimkan hadiah,<br></br>{" "}
         silahkan mengirimkannya melalui tautan berikut
-      </p>
+      </p> */}
       <div className="logo-gift">
         <a>
-          <img src="gift-box.png" />
-        </a>
-        <a>
-          <img src="wallet.png" />
+          <img src="bank.png" />
+          <p>
+            a.n Renaldi<br></br>BCA : 2950234761 a/n RENALDI
+          </p>
         </a>
       </div>
     </div>
