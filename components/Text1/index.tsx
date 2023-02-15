05be34687610812e054417cfeab8c6008c6b558c
renaldia63@gmail.com <renaldia63@gmail.com>
2023-02-15 08:08:59 +0700

    update text

--- components/Text1/index.tsx
+++ components/Text1/index.tsx
@@ -2,8 +2,8 @@ export default function Text1() {
   return (
     <div className="text-marriage">
       <p>
-        “Happy marriages begin when we marry the ones we love, and they blossom
-        when we love the ones we marry.”
+        “Happy marriages begin when we marry the ones we love, and<br></br> they
+        blossom when we love the ones we marry.”
       </p>
     </div>
   );
