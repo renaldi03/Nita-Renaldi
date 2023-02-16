6b58723cf024e7897cf08914093ed2f086de7584
renaldia63@gmail.com <renaldia63@gmail.com>
2023-02-16 11:07:53 +0700

    update

--- components/Text2/index.tsx
+++ components/Text2/index.tsx
@@ -2,8 +2,8 @@ export default function Text2() {
   return (
     <div className="text-quote2">
       <p data-aos="fade-up">
-        "being someone's first love may be great, but to be thier last <br></br>
-        is beyond perfect."
+        "being someone's first love may be great, but to be thier last is beyond
+        perfect."
       </p>
       <img src="two-lines.png" />
     </div>
