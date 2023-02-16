6a2c07d96536d4926853482a26f82997c7d51f8c
renaldia63@gmail.com <renaldia63@gmail.com>
2023-02-16 22:11:54 +0700

    update

--- components/maps/index.tsx
+++ components/maps/index.tsx
@@ -5,6 +5,7 @@ export default function Maps() {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.9602644536106!2d106.04027336218824!3d-6.016570976454352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e418e1557b7d25d%3A0x94e6d915b4a09b26!2sAula%20Al-khairiyah%20Citangkil%2C%20Cilegon!5e0!3m2!1sid!2sid!4v1676480814093!5m2!1sid!2sid"
         loading="lazy"
+        href-="https://goo.gl/maps/3W2MR63MQFaAGmoz8"
       ></iframe>
     </div>
   );
