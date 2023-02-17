2ca19cb17494a3584a10b951fc457126401940ed
renaldia63@gmail.com <renaldia63@gmail.com>
2023-02-17 11:00:41 +0700

    test

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,6 +4,7 @@ export default function Header(): JSX.Element {
       <div className="shadow">
         <div className="text-header1">
           <img src="our.png" />
+          <img src="header1.png" />
           <h1>Nita & Aldi</h1>
           <h2>25 February 2023</h2>
         </div>
